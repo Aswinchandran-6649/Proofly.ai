ff0384f9ad7036ec3b13e6dc71e7b8001295ab9b
Aswinchandran-6649 <aswinchandran6649@gmail.com>
2026-03-09 13:25:29 +0530

    Fix production bug

--- server/controller/user/aiController.js
+++ server/controller/user/aiController.js
@@ -1,13 +1,13 @@
 const { extractReceiptData } = require("../../services/geminiAi");
 
-// Handle Receipt Scanning
+
 exports.scanReceiptController = async (req, res) => {
   try {
     if (!req.file) {
       return res.status(400).json({ error: "No image file provided" });
     }
 
-    // Heavy lifting is done by the service
+  
     const extractedData = await extractReceiptData(
       req.file.buffer,
       req.file.mimetype
